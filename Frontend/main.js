0b181a370c73d9156bebdf2a53eed3ff5ab562ba
Izan <sanchezginesizan@gmail.com>
2025-05-16 20:32:05 +0200

    .

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,36 +1,40 @@
-const form = document.querySelector('.main-container__form')
+document.addEventListener('DOMContentLoaded', () => {
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+    const form = document.querySelector('.main-container__form')
 
-    const username = e.target[0].value;
-    const password = e.target[1].value;
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-    // Realizar la solicitud de inicio de sesión
-    const res = await fetch('https://sculpin-pro-newly.ngrok-free.app/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
+        const username = e.target[0].value;
+        const password = e.target[1].value;
+
+        // Realizar la solicitud de inicio de sesión
+        const res = await fetch('https://sculpin-pro-newly.ngrok-free.app/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (!res.ok) {
+            throw new Error('Usuario o contraseña incorrectos');
+        }
+
+        // Obtener los datos de la respuesta
+        const data = await res.json();
+        const token = data.access_token;
+        const role = data.role;
+
+        // Guardar el token en localStorage
+        localStorage.setItem('token', token);
+        
+        // Redirigir según el rol
+        if (role === 'parent') {
+            window.location.href = '/admin';
+        } else {
+            window.location.href = '/child';
+        }
     });
-
-    if (!res.ok) {
-        throw new Error('Usuario o contraseña incorrectos');
-    }
-
-    // Obtener los datos de la respuesta
-    const data = await res.json();
-    const token = data.access_token;
-    const role = data.role;
-
-    // Guardar el token en localStorage
-    localStorage.setItem('token', token);
     
-    // Redirigir según el rol
-    if (role === 'parent') {
-         window.location.href = '/admin';
-     } else {
-         window.location.href = '/child';
-     }
 });
